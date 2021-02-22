ebf3b218c457b2ed571d006b025b27eb5644bbd8
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-02-22 21:32:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import TinyMCE from "react-tinymce";
 
 function App() {
+  const [body, setBody] = useState("");
+  const onChange = (e) => {
+    setBody(e.target.getContent());
+  };
+  const setData = (e) => {};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TinyMCE onChange={onChange}></TinyMCE>
+      <div>{body}</div>
     </div>
   );
 }
